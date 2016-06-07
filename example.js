8952d599194e3d35355241165fbb3020b21f41a7
Franklin van de Meent <fr@nkl.in>
2016-06-07 22:09:25 +0200

    Chore(package): Add example.js for Tonic

---
+++ example.js
@@ -0,0 +1,13 @@
+// Load module
+var pwned = require ('haveibeenpwned') ();
+
+// Fancy console.log
+function output (err, data) {
+  console.dir (err || data, {
+    depth: null,
+    colors: true
+  });
+}
+
+// Get all Linkedin breaches
+pwned.breaches ({ domain: 'linkedin.com' }, output);
