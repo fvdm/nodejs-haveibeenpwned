850f6782b1ae3d58eace871e7b84fdb0829a94e9
Franklin van de Meent <fr@nkl.in>
2016-06-07 21:47:54 +0200

    Test(runner): Verbose coverage commands

--- test.sh
+++ test.sh
@@ -4,8 +4,8 @@ result=0
 eslint *.js || result=1
 
 if [ "$TRAVIS" == "true" ]; then
-  istanbul cover test.js --print none --report lcovonly || result=1
-  [ "$result" -eq "0" ] && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js || result=1
+  istanbul cover test.js --report lcovonly || result=1
+  [ "$result" -eq "0" ] && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js -v || result=1
 else
   node test.js || result=1
 fi
